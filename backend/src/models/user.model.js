2ddb8e057eee0d78f195a217e0765e781d2f6a53
Yodahe Teshome <jodahe2001@gmail.com>
2025-08-18 13:13:08 +0300

    make default users to be owner when registering

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       enum: ['admin', 'owner', 'public'],
-      default: 'public'
+      default: 'owner'
     },
     password: {
       type: String,
